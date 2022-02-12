e409fc37257a99339198e491528b9d8c590dc7c5
ahrii <ria.i@daum.net>
2022-02-12 21:08:44 +0900

    Update: tokenizer import

--- tool/tokenizer/base.py
+++ tool/tokenizer/base.py
@@ -2,11 +2,13 @@ class BaseTokenizer:
     sample = "결국 황대헌만 부지런히 쫓아갔더니 은메달이라는 값진 성과를 냈다는 것이다."
     
     def tokenize(self, text):
-        return text
+        return text.split()
     
     def token_to_sent(self, token:list) -> str:
         return ' '.join(token)
     
-    def toCharacter(self, text):
-        """ character-level tokenization """
+ 
+    
+class Character(BaseTokenizer):
+    def tokenize(self, text):
         return [char for char in text if char != ' ']
--- tool/tokenizer/koNLPy.py
+++ tool/tokenizer/koNLPy.py
@@ -1,34 +1,39 @@
 from .base import BaseTokenizer
 
+import konlpy
+from konlpy.tag import *
 
-class KoNLPy(BaseTokenizer):
-    global msg
-    msg = {'WrongTokenizerName':'KoNLPy does not support such tokenizer!',
+
+
+_TOKENIZERS = ['Kkma', 'Okt', 'Mecab', 'Komoran','Hannanum']
+TOKENIZER_DEFAULT = _TOKENIZERS[2]
+
+MSG = {'WrongTokenizerName':'KoNLPy does not support such tokenizer!',
           'KkmaOnly':'Only possible with Kkma!'}
 
-    def __init__(self, tokenizer):
-        try:
-            from konlpy.tag import *
-        except(ImportError):
-            raise ImportError(
-                "\n\nPlease install KoNLPy library for KoNLPy tokenization:"
-            )            
-        self.tokenizers = {'kkma': Kkma, 'okt': Okt, 'mecab': Mecab,
-                          'komoran': Komoran, 'hannanum': Hannanum}
-        if tokenizer not in self.tokenizers.keys():
-            raise ValueError(msg['WrongTokenizerName'])
-            
-        self.tokenizer = self.tokenizers[tokenizer]
+def get_tokenizer(name: str):
+    return getattr(konlpy.tag, name.title())
+
+
+class KoNLPy(BaseTokenizer):
+    def __init__(self, name: str = TOKENIZER_DEFAULT):
+        if name is None:
+            the_tokenizer = TOKENIZER_DEFAULT
+        else:
+            assert name in _TOKENIZERS, MSG['WrongTokenizerName'] 
+            the_tokenizer = name
+
+        self.tokenizer = get_tokenizer(the_tokenizer)()
         
         
     def tokenize(self, sentence:str) -> list:
         """ morpheme-level tokenization """
-        return self.tokenizer().morphs(sentence)
+        return self.tokenizer.morphs(sentence)
     
     
     def split_sentence(self, text:str) -> list:
-        assert self.tokenizers['kkma'] == self.tokenizer, msg['KkmaOnly']
-        return self.tokenizer().sentences(text)
+        assert self.tokenizers['kkma'] == self.tokenizer, MSG['KkmaOnly']
+        return self.tokenizer.sentences(text)
     
     
     def __call__(self):
