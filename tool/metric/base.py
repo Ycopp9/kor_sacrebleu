a10ce57e82c5f36a8ab837cfa087cd5da0612168
ahrii <ria.i@daum.net>
2022-02-14 17:55:14 +0900

    Completed: Sacrebleu

--- tool/metric/base.py
+++ tool/metric/base.py
@@ -4,11 +4,16 @@ from typing import Sequence, Optional
 class Metric:
     def __init__(self, 
                  hypothesis: str = None,
-                references: Optional[Sequence[Sequence[str]]] = None):
+                 references: Optional[Sequence[Sequence[str]]] = None,
+                obj: object = None):
         self.hypothesis = hypothesis
         self.references = references
+        self.obj = obj
         
+    def get_method(self, method_name: str):
+        return getattr(self.obj, method_name)
     
+
     def get_score(self, method_name, corpus_level=False):
         if corpus_level:
             self.hypothesis = [self.hypothesis]
--- tool/metric/sacreBLEU.py
+++ tool/metric/sacreBLEU.py
@@ -1,22 +1,39 @@
-from .baseSacre import BaseSacre
+from .base import Metric
+from typing import Sequence, Optional
 
+NAME = ['bleu', 'chrf', 'ter']
 
-class SacreBLEU(BaseSacre):
-    def __init__(self, name: str):        
-        assert name.lower() in ['bleu', 'chrf', 'ter'], 'Metric not available!'
+class SacreBLEU(Metric):
+    def __init__(self,
+                 name: str,
+                force: bool = False,
+                smooth_method: str = 'exp',
+                smooth_value: Optional[float] = None,
+                effective_order: bool = False):
+        super().__init__()
+        
+        assert name.lower() in NAME, 'Metric not available!'
         self.name = name.lower()
+        self.force = force
         if self.name == 'bleu': 
             self.force = True ###
-            
+        self.smooth_method = smooth_method
+        self.smooth_value = smooth_value
+        self.effective_order = effective_order
+        
+        try:
+            import sacrebleu
+        except:
+            ImportError
+        self.obj = sacrebleu
         self.method_sentence = f'sentence_{self.name}'
         self.method_corpus = f'corpus_{self.name}'
         
         
-    def compute_score(self, sys_level=False) -> float:
+    def get_score(self, sys_level=False) -> float:
         if sys_level:
-            method = self.method_corpus
-            corpus_level = True
+            return super().get_score(method_name=self.method_corpus,
+                                    corpus_level = True) 
         else:
-            method = self.method_sentence
-            corpus_level = False
-        return super().get_score(method, corpus_level)
+            return super().get_score(method_name=self.method_sentence,
+                                 corpus_level=False)
